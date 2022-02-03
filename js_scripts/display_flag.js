86471fe6cbc2ca644f24ca4ffaaf0b2f3ff0864d
Eli Ogilvy <eogilvy@seattleu.edu>
2022-02-02 16:40:46 -0800

    User and password validation function added

--- js_scripts/display_flag.js
+++ js_scripts/display_flag.js
@@ -1,4 +1,17 @@
 
-function print_flag() {
+function do_something() {
     alert("flag{y0u_f0und_yet_an0th3er_fl4g}");
+}
+function login() {
+    var user = document.getElementById('uname').value;
+    var pass = document.getElementById('psw').value;
+    if (user === 'admin' && pass === 'password'){
+        window.location='/pages/login_success.html'
+    }
+    else if (user !== 'admin'){
+        document.getElementById('error').innerHTML = "Incorrect Username"
+    }
+    else{
+        document.getElementById('error').innerHTML = "Incorrect Password"
+    }
 }
