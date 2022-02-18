e6ec64e6620f45cc71d2fdc94659a0879b4d08c9
YL-Tan <77035911+YL-Tan@users.noreply.github.com>
2022-02-18 01:20:45 -0800

    Update content

--- RunMe.py
+++ RunMe.py
@@ -1,3 +1,5 @@
+import random
+import sys
 # Hiding this really important number in an obscure piece of code is brilliant!
 # AND it's encrypted!
 # We want our biggest client to know his information is safe with us.
@@ -24,8 +26,25 @@ def choose_greatest():
 
     print( "The number with largest positive magnitude is "
         + str(greatest_value) )
+    
+def print_encouragement():
+    encouragements = ['You can do it!', 'Keep it up!', 
+                        'Look how far you\'ve come!']
+    choice = random.choice(range(0, len(encouragements)))
+    print('\n-----------------------------------------------------')
+    print(encouragements[choice])
+    print('-----------------------------------------------------\n\n')
 
-def decode_secret(secret):
+def decode_secret(secret, key):
+    #extend key to secret length
+    new_key = key
+    i = 0
+    while len(new_key) < len(secret):
+        new_key = new_key + key[i]
+        i = (i + 1) % len(key)        
+    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c,new_key_c) in zip(secret,new_key)])
+
+def str_decode(secret):
     """ROT47 decode
 
     NOTE: encode and decode are the same operation in the ROT cipher family.
@@ -44,4 +63,49 @@ def decode_secret(secret):
 
     print(decoded)
 
-choose_greatest()
+def main():
+
+  print(
+'''
+    Y
+  .-^-.
+ /     \      .- ~ ~ -.
+()     ()    /   _ _   `.                     _ _ _
+ \_   _/    /  /     \   \                . ~  _ _  ~ .
+   | |     /  /       \   \             .' .~       ~-. `.
+   | |    /  /         )   )           /  /             `.`.
+   \ \_ _/  /         /   /           /  /                `'
+    \_ _ _.'         /   /           (  (
+                    /   /             \  \\
+                   /   /               \  \\
+                  /   /                 )  )
+                 (   (                 /  /
+                  `.  `.             .'  /
+                    `.   ~ - - - - ~   .'
+                       ~ . _ _ _ _ . ~
+'''
+  )
+  print('Welcome to the CPSC4710/5710 CTF encourager!\n\n')
+  
+  while True:
+    print('a) Print encouragement')
+    print('b) Print flag')
+    print('c) Quit\n')
+    choice = input('What would you like to do? (a/b/c) ')
+    
+    if choice == 'a':
+      print_encouragement()
+      
+    elif choice == 'b':
+      choose_greatest()
+      print('\nOh Shoot! I must have misplaced the print flag function! Check my source code!\n\n')
+      
+    elif choice == 'c':
+      sys.exit(0)
+      
+    else:
+      print('\nI did not understand "' + choice + '", input only "a", "b" or "c"\n\n')
+
+
+if __name__ == "__main__":
+  main()
